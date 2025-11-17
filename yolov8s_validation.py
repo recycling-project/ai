413e55f65ca6b45d2fe5cdabcc0d87422167d71d
DuFrump <jihoon6052@gmail.com>
2025-11-17 14:53:44 +0900

    yolo_validation 코드 수정

--- yolov8s_validation.py
+++ yolov8s_validation.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
 import cv2
 
-
 def validation_model():
     model = YOLO('./eco_scan/eco_scan_weight/weights/best.pt') 
     results = model.predict('./val_img/my_test_image.jpg', save=True) 
